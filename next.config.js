bc08a19a2971ae3454588c052d5be61404fc4f36
레골람 <119289560+legolam@users.noreply.github.com>
2025-03-04 20:19:33 +0900

    젭라

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath: "/nextjs-character-generator", // GitHub Pages용 경로 설정 (저장소 이름)
-  assetPrefix: "/nextjs-character-generator/", // 정적 파일 경로 설정
+  basePath: "/fantasy_world", // 저장소 이름과 동일하게 설정
+  assetPrefix: "/fantasy_world/", // 정적 파일 경로 설정
 };
 
 module.exports = nextConfig;
